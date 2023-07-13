5634ce83f9aeba2d58eeb5df706ad7b291a9118b
Francisco Dominguez <franciscoandresdominguez@hotmail.com>
2023-07-13 17:10:10 -0300

    Cuadro de Error

--- proyecto-frontend/src/componentes/registrarse/registrarse.js
+++ proyecto-frontend/src/componentes/registrarse/registrarse.js
@@ -1,23 +1,54 @@
 import React, { useState } from 'react';
 import '../../assets/global.scss';
 import '../background/background.scss';
+import ErrorModal from '../../errorFolder/errores';
+import Card from '../../errorFolder/card';
 
 export default function Registrarse() {
-  const [inputValue, setInputValue] = useState('');
-  const [inputValue2, setInputValue2] = useState('');
-  const [inputValue3, setInputValue3] = useState('');
-  const [inputValue4, setInputValue4] = useState('');
+  const [ingresarNombre, setIngresarNombre] = useState('');
+  const [ingresarCorreo, setIngresarCorreo] = useState('');
+  const [ingresarUsuario, setIngresarIngresarUsuario] = useState('');
+  const [invalid, setInvalid] = useState(false);
+  const [ingresarContrasenia, setIngresarIngresarContrasenia] = useState('');
 
-  const handleInputChange = (e, setInputValue) => {
-    setInputValue(e.target.value);
+  const [error, setError] = useState({
+    title: '',
+    message: ''
+  })
+
+  const handleInputChange = (e, setIngresarNombre) => {
+    setIngresarNombre(e.target.value);
   };
 
-  const isInputFilled = inputValue.trim() !== '';
-  const isInputFilled2 = inputValue2.trim() !== '';
-  const isInputFilled3 = inputValue3.trim() !== '';
-  const isInputFilled4 = inputValue4.trim() !== '';
+  const isInputFilled = ingresarNombre.trim() !== '';
+  const isInputFilled2 = ingresarCorreo.trim() !== '';
+  const isInputFilled3 = ingresarUsuario.trim() !== '';
+  const isInputFilled4 = ingresarContrasenia.trim() !== '';
+
+  function okay(){
+    setInvalid(false);
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    if(ingresarCorreo.trim().length === 0 || ingresarNombre.trim().length === 0 || ingresarUsuario.trim().length === 0){
+      setError({
+        title: 'Campo vacío',
+        message: 'Faltó rellenar algún valor, revise el formulario y envíelo devuelta.'
+      })
+      setInvalid(true);
+      return;
+    }
+  }
 
   return (
+
+    <div>
+
+    {invalid && <ErrorModal title={error.title} message={error.message} onClick ={okay} ></ErrorModal>}
+    <Card className='input' >
+    <form  onSubmit={handleSubmit}>
     <div className="gradient-background">
       <div className="green-rectangle">
         <span className="text">AGROIA</span>
@@ -28,35 +59,44 @@ export default function Registrarse() {
           className="sub-rectangle-first"
           type="text"
           placeholder="Ingrese su nombre"
-          value={inputValue}
-          onChange={(e) => handleInputChange(e, setInputValue)}
+          value={ingresarNombre}
+          onChange={(e) => handleInputChange(e, setIngresarNombre)}
           style={{ color: isInputFilled ? 'black' : '#888' }}
         />
         <input
           className="sub-rectangle"
           type="text"
           placeholder="Ingrese su correo electrónico"
-          value={inputValue2}
-          onChange={(e) => handleInputChange(e, setInputValue2)}
+          value={ingresarCorreo}
+          onChange={(e) => handleInputChange(e, setIngresarCorreo)}
           style={{ color: isInputFilled2 ? 'black' : '#888' }}
         />
         <input
           className="sub-rectangle"
           type="text"
           placeholder="Ingrese su nombre de usuario"
-          value={inputValue3}
-          onChange={(e) => handleInputChange(e, setInputValue3)}
+          value={ingresarUsuario}
+          onChange={(e) => handleInputChange(e, setIngresarIngresarUsuario)}
           style={{ color: isInputFilled3 ? 'black' : '#888' }}
         />
         <input
           className="sub-rectangle"
           type="text"
           placeholder="Ingrese su contraseña"
-          value={inputValue4}
-          onChange={(e) => handleInputChange(e, setInputValue4)}
+          value={ingresarContrasenia}
+          onChange={(e) => handleInputChange(e, setIngresarIngresarContrasenia)}
           style={{ color: isInputFilled4 ? 'black' : '#888' }}
         />
+        <button className="green-button">Registrarse</button>
+        
       </div>
+      
     </div>
+    </form>
+    </Card>
+    </div>
+
   );
 }
+
+
