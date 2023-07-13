17dde8fc8171b197c46edce3e4e422fdbff6c0b2
Camila Guillan <camiguillan@gmail.com>
2023-07-13 16:01:40 -0300

    agregado de inputs agregar campo

--- proyecto-frontend/src/componentes/agregarCampo/agregarCampo.js
+++ proyecto-frontend/src/componentes/agregarCampo/agregarCampo.js
@@ -2,13 +2,26 @@ import React from 'react'
 import Header from '../reusable/header/header';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import AddIcon from '../../assets/icons/addIcon';
+import "./agregarCampo.scss";
+import Card from '../reusable/card/card';
 
 
 export default function AgregarCampo() {
   return (
     <div>
       <Header></Header>
-     <AddIcon></AddIcon>
+      <h2 className='agregar-campo-titulo'> <AddIcon></AddIcon> AGREGAR CAMPO</h2>
+      <Card className={"agregar-campo-container input"}>
+        <form>
+          <input placeholder='Inserte el nombre del campo'   />
+          <input placeholder='Inserte el ancho del campo'   />
+          <input placeholder='Inserte el largo del campo'   />
+          <input placeholder='Inserte una imagen del campo'   />
+       
+
+        </form>
+     
+        </Card>
     </div>
   )
 }
---
+++ proyecto-frontend/src/componentes/reusable/card/card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./card.scss";
+
+export default function Card(props) {
+  return (
+    <div className={props.className}>
+        {props.children}
+    </div>
+  )
+}
