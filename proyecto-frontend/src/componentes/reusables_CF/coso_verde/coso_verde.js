26b740ae2ea9308ee8146b8833956b68e89a3464
Carolina Blanco <cablanco@frba.utn.edu.ar>
2023-07-13 16:37:15 -0300

    Iniciar sesion casi listo, solo falta el boton

---
+++ proyecto-frontend/src/componentes/reusables_CF/coso_verde/coso_verde.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./coso_verde.scss";
+//import AgroIcon from '../../../assets/icons/agroIcon'; ->DESCOMENTAR CUANDO ESTE EL ICON
+
+
+//<AgroIcon></AgroIcon> -> DESCOMENTAR CUANDO ESTE EL ICON Y AGREGAR
+export default function CosoVerde() {
+    return (
+        <div className="green-rectangle">
+            
+            <span className="text">AGROIA</span>
+        </div>
+    )
+  }
---
+++ proyecto-frontend/src/componentes/reusables_CF/white_container/white_container.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import '../../../assets/global.scss';
+import '../white_container/white_container.scss';
+
+
+
+export default function WhiteContainer() {
+
+    return (
+    
+        <div className="white-rectangle">
+            <span className="container-text">Inicia Sesion</span>
+        </div>
+  )
+}
