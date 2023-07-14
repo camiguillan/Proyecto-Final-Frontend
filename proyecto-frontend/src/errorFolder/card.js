4a12e2043efe3d303b26fec354f5cebe485e65ea
Camila Guillan <camiguillan@gmail.com>
2023-07-13 21:43:40 -0300

    cambio css a scss

--- proyecto-frontend/src/errorFolder/card.js
+++ proyecto-frontend/src/errorFolder/card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './card.css';
+import './card.scss';
 
 
 export default function Card(props) {
--- proyecto-frontend/src/errorFolder/errores.js
+++ proyecto-frontend/src/errorFolder/errores.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './error.css';
+import './error.scss';
 import Card from './card';
 import { useState } from 'react';
 import Button from './button';
