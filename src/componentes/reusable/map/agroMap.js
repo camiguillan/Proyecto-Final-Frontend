d63e45887c7c8a770e174e435bb06fb691403f7a
Tomas Molino <totomolino@hotmail.com>
2023-07-18 15:47:55 -0300

    fix: coordinates order

--- src/componentes/reusable/map/agroMap.js
+++ src/componentes/reusable/map/agroMap.js
@@ -66,22 +66,24 @@ function AgroMap({ coordinates }) {
 
       const coord1 = Number(matches[1]);
       const coord2 = Number(matches[2]);
-      const geocodes = [];
 
-      if (coord1 < -90 || coord1 > 90) {
-        // must be lng, lat
-        geocodes.push(coordinateFeature(coord1, coord2));
-      }
+      console.log(`Coord1: ${coord1} Coord2: ${coord2}`);
+      const geocodes = [];
 
       if (coord2 < -90 || coord2 > 90) {
         // must be lat, lng
         geocodes.push(coordinateFeature(coord2, coord1));
       }
 
+      if (coord1 < -90 || coord1 > 90) {
+        // must be lng, lat
+        geocodes.push(coordinateFeature(coord1, coord2));
+      }
+
       if (geocodes.length === 0) {
         // else could be either lng, lat or lat, lng
-        geocodes.push(coordinateFeature(coord1, coord2));
         geocodes.push(coordinateFeature(coord2, coord1));
+        geocodes.push(coordinateFeature(coord1, coord2));
       }
 
       return geocodes;
