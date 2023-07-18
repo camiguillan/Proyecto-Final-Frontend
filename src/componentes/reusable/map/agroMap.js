9f7411facf95995db2717c43476399dbe47f13d2
Camila Guillan <camiguillan@gmail.com>
2023-07-18 12:06:54 -0300

    feat: added marker to map (mapa de cacas))

--- src/componentes/reusable/map/agroMap.js
+++ src/componentes/reusable/map/agroMap.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from 'mapbox-gl-geocoder'; // SEARCH BAR
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
@@ -12,14 +12,16 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2FtaWd1aWxsYW4iLCJhIjoiY2xrNXNvcHdpMHg4czNzb
 
 function AgroMap({ coordinates }) {
   const mapContainer = useRef(null);
+  const [searchedCoordinates, setSearchedCoordinates] = useState([-58.702963, -34.671792]);
 
   useEffect(() => {
+    console.log(searchedCoordinates);
     const map = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/satellite-v9',
-      center: [-58.702963, -34.671792], // Default center coordinates
-      zoom: 12, // Default zoom level
-    });
+      center: searchedCoordinates, // Default center coordinates
+      zoom: 15, // Default zoom level
+    }, [searchedCoordinates]);
 
     const drawOptions = {
       displayControlsDefault: false,
@@ -38,6 +40,7 @@ function AgroMap({ coordinates }) {
     map.addControl(nav, 'bottom-right');
 
     const geocoderContainer = () => <div id="geocoder-container" className="geocoder-container" style={{ width: '100%', borderRadius: '10px' }} />;
+
     // SEARCH BAR
     const geocoder = new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
@@ -45,18 +48,34 @@ function AgroMap({ coordinates }) {
       countries: 'AR',
       bbox: [-73.560222, -55.057499, -53.637810, -21.781235], // Limitamos resultados a solo arg
       container: geocoderContainer,
+      marker: {
+        color: 'red',
+      },
     });
 
     map.addControl(geocoder, 'top-left');
 
     map.on('result', (event) => {
       const { result } = event;
+      console.log(result);
 
       // Retrieve the coordinates from the geocoding result
       const { center } = result.geometry;
+      setSearchedCoordinates(center);
 
       // Center the map to the selected location
       map.setCenter(center);
+    });
+
+    const marker = new mapboxgl.Marker({ draggable: false, color: 'red' });
+    geocoder.on('result', (e) => {
+      marker.remove(map);
+      console.log(e.result.center);
+      // eslint-disable-next-line no-underscore-dangle
+      marker
+        .setLngLat(e.result.center)
+        .addTo(map);
+      map.flyTo({ center: e.result.center, zoom: 17 });
     });
 
     function handleDraw() {
