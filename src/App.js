8f235fcce5f35aa86f8b0e860f204a2021a181c4
Carolina Blanco <cablanco@frba.utn.edu.ar>
2023-08-02 12:13:56 -0300

    Deleted "dashboards" folder from App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import EditarCampo from './componentes/editarCampo/editarCampo';
 import Home from './componentes/home/home';
 import IniciarSesion from './componentes/iniciarSesion/iniciarSesion';
 import Registrarse from './componentes/registrarse/registrarse';
-import Dashboards from './componentes/dashboards/dashboards';
 import OlvidoContra from './componentes/olvidoContra/olvidoContra';
 import EditarPerfil from './componentes/editarPerfil/editarPerfil';
 import VerCultivos from './componentes/verCultivos/verCultivos';
@@ -29,7 +28,6 @@ function App() {
         <Route path="/home/:userID" element={<Home />} />
         <Route path="/iniciarSesion" element={<IniciarSesion />} />
         <Route path="/registrarse" element={<Registrarse />} />
-        <Route path="/campo/dashboards/:userID" element={<Dashboards />} />
         <Route path="/olvidoContra" element={<OlvidoContra />} />
         <Route path="/recover_password/:userID" element={<RecuperarContra />} />
         <Route path="/editarPerfil/:userID" element={<EditarPerfil />} />
