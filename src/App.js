c3cc2af389d24a183d77977347d3b4271cde6254
Camila Guillan <camiguillan@gmail.com>
2023-08-02 19:45:44 -0300

    feat: map container abstraction for create and edit field

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import React from 'react';
-import AgregarCampo from './componentes/agregarCampo/agregarCampo';
+import AgregarCampo from './componentes/agregarCampo2/agregarCampo';
 import AgregarLotes from './componentes/agregarLotes/agregarLotes';
 import EditarCampo from './componentes/editarCampo/editarCampo';
 import Home from './componentes/home/home';
--- src/componentes/editarCampo/editarCampo.js
+++ src/componentes/editarCampo/editarCampo.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import Header from '../reusable/header/header';
+import Icon from '../../assets/icons/icon';
+import MapContainer from '../reusable/mapContainer/mapContainer';
 
 export default function EditarCampo() {
   return (
     <div>
       <Header />
-      <div>EditarCampo</div>
+      <h1 className="agregar-campo-titulo">
+        {' '}
+        <Icon className="bi bi-pencil-square" color="#464E47" fontSize="" />
+        {' '}
+        EDITAR CAMPO
+      </h1>
+      <MapContainer
+        campInfo={{
+          nombreCampo: '',
+          imagen: '',
+          coordinates: [-58.44657291015618, -34.595435305682834],
+          features: [],
+        }}
+        cultivosSeleccionados={['']}
+        feats={[]}
+        campoPrincipal={{
+          geometry: [],
+          id: '',
+          properties: {},
+          type: '',
+        }}
+        edit
+      />
     </div>
   );
 }
