8db0fcc564f6a45a671a17d883d0740a68d9eeef
Camila Guillan <camiguillan@gmail.com>
2023-08-09 20:41:07 -0300

    fix: loader color

--- src/componentes/reusable/loader/loader.js
+++ src/componentes/reusable/loader/loader.js
@@ -5,7 +5,7 @@ import Icon from '../../../assets/icons/icon';
 export default function Loader() {
   return (
     <div className="loader2">
-      <Icon className="bi bi-flower1 flower-loader" color="#439775" fontSize="7rem" />
+      <Icon className="bi bi-flower1 flower-loader" color="#2a7d2e" fontSize="7rem" />
     </div>
   );
 }
