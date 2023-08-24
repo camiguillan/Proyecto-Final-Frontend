e933e07ae558ad3ecb2359e5ede2a3d4856bcd48
Camila Guillan <camiguillan@gmail.com>
2023-08-24 09:43:24 -0300

    fix: gray color when disabled

--- src/componentes/reusable/mapContainer/mapContainer.js
+++ src/componentes/reusable/mapContainer/mapContainer.js
@@ -152,7 +152,7 @@ export default function MapContainer({
     <label key={index} className={!edit ? 'agregar-campo-label' : 'agregar-campo-label-edit'}>
       Tipo de cultivo:
       <select
-        className="select"
+        className={`select ${edit ? 'disabled' : ''}`}
         value={cultivo}
         onChange={(e) => handleChange(e.target.value, index)}
         disabled={edit}
